2640fbc86322f2befcc7048c9f71085da181b423
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2024-07-05 22:13:33 -0600

    fixed method name

--- app/Traits/Sortable.php
+++ app/Traits/Sortable.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 trait Sortable
 {
-    protected static function bootSorted()
+    protected static function bootSortable()
     {
         static::creating(function (Model $record) {
             if ($record->sort === null) {
